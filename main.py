8e5ca2bc65b8821a57f9c8d47858f5dfd5bf2523
JackSong88 <86933572+JackSong88@users.noreply.github.com>
2024-02-10 12:59:26 -0500

    User info

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/user_info')
+def user_info():
+    return render_template('user.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 #### MIn peng
