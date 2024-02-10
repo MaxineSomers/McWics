2ac3750d6300efc3ed5793a05068fc40d42921a9
MaxineSomers <maxine.m.somers@gmail.com>
2024-02-10 12:35:05 -0500

    basic form for input

--- maxineTest.py
+++ maxineTest.py
@@ -4,13 +4,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def add(): #returns template of a form to accwept expense info
+def add():
     return render_template('add.html')
-
-# Define a route and its corresponding view function
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
 # Run the Flask application
 if __name__ == '__main__':
