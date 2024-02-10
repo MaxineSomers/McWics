a6111015567f6aa758fc541de25fcad9df56ce63
Min Peng <tobylutina@gmail.com>
2024-02-10 13:00:59 -0500

    test

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
+@app.route('/Monica Page')
+def min_page():
+    return render_template('min_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 #### MIn peng
